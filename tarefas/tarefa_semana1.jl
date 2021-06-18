93d35e754baef609bbaf1297bc9eab19e172de4c
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-06-18 15:43:44 -0300

    Updates Tarefa 1 com link

--- tarefas/tarefa_semana1.jl
+++ tarefas/tarefa_semana1.jl
@@ -6,15 +6,18 @@ using InteractiveUtils
 
 # ╔═╡ dbbdbf3a-d063-11eb-062f-7fd71304eb46
 md"""
-## Tarefa Semana 1
+#### Tarefa Semana 1
 
-> Implementar em Julia um algoritmo para encontrar raiz quadrada de um número interio ${n >0}$. 
+> Implementar em Julia os seguintes algorimtos para encontrar raiz quadrada de um número inteiro $${n >0}$$. 
 >  1. Com 1 casa de precisão depois da vírgula
->  2. Com $t\in\mathbb{Z}$ casas de precisão depois da vírgula
+>  2. Com $$t\in\mathbb{Z}$$ casas de precisão depois da vírgula
 
 **Observações:** 
-- Salve este notebook Pluto com nome `Tarefa1_Meu_Nome.pluto.jl` e faça o upload do mesmo na Tarefa 1 - Semana 1 no Moodle
+- Suba o Notebook Pluto com nome _Tarefa1_Meu_Nome.pluto.jl_ na Tarefa 1 específica no Julia
+
 - Não esqueça de incluir as *Referências* consultadas, incluindo pessoas.
+
+- Você abrir diretamente no Pluto a tarefa usando o seguinte [link](https://github.com/lrsantos11/metodos-numericos/raw/2021-1/tarefas/tarefa_semana1.jl)
 """
 
 # ╔═╡ Cell order:
