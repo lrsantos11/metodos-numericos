db77ff54e97308fd25f85653d3038b4004f1d885
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-06-23 15:22:12 -0300

    Updates SE02A01

--- notebooks/Semana02-Aula1-IntroJulia.pluto.jl
+++ notebooks/Semana02-Aula1-IntroJulia.pluto.jl
@@ -6,26 +6,25 @@ using InteractiveUtils
 
 # ╔═╡ b25c1fd4-b6e2-4248-8bb9-41a1f06032dd
 begin
-	import Pkg; Pkg.add("DataFrames")
-	using DataFrames
 	using LinearAlgebra
 end
 
 # ╔═╡ a2d2d41c-d397-11eb-0f7d-ed2891f49049
 md"""
-###### UFSC/Blumenau
-###### MAT1831 - Métodos Numéricos
-###### Prof. Luiz-Rafael Santos
-"""
-
-# ╔═╡ 4d8839cb-f01e-47b0-9671-3eb4f58e5acf
-md"""
-- Vamos primeiro instalar pacotes do Julia usados na aula
+##### UFSC/Blumenau
+##### MAT1831 - Métodos Numéricos
+##### Prof. Luiz-Rafael Santos
+###### Semana 02 - Aula 01
 """
 
 # ╔═╡ 09cb6899-07c6-436e-808f-b85f2c0f04de
 md"""
 # Introdução a Julia (continuação)
+"""
+
+# ╔═╡ 4d8839cb-f01e-47b0-9671-3eb4f58e5acf
+md"""
+- Vamos primeiro instalar pacotes do Julia usados na aula
 """
 
 # ╔═╡ 34ad60f7-8c7b-464c-8a67-1e9e6a562f84
@@ -163,19 +162,6 @@ D .* E #Broadcast - .+Função
 # ╔═╡ d0014623-085d-4162-b164-4ab360c84f18
 sind.(D)
 
-# ╔═╡ ee4d5876-43ee-4f5a-951a-0ea3114ded2d
-I
-
-# ╔═╡ 252c34fc-f179-45d9-8864-fc564ba013e5
-D*I == D
-
-# ╔═╡ aa5c435e-a069-452c-8a7f-d3ec19b844c6
-F = [D  exp2.(E);
- E'    I     ]	
-
-# ╔═╡ fc7fc26e-15b9-47f2-861a-ac8793e0b1f0
-m, n = size(F)
-
 # ╔═╡ 80d793be-5d74-4254-8359-d5f029d203a4
 md"""
 ## Funções
@@ -203,54 +189,11 @@ func1 = x -> sin(x)*cos(x)
 # ╔═╡ edadaf37-303c-44b9-b73f-f972a9097198
 func1(-3)
 
-# ╔═╡ 206f3485-ba82-4b9e-948a-97e656f42b8a
-md"""
-# Erros em Algorimos numéricos
-## Erros relativos e absolutos
-Inevitavelmente, encontraremos erros quando implementamos algum algoritmos numéricos. Isto porque tipicamente nosso objetivo é encontrar uma _aproximação $v$_ de alguma _solução exata $u$_. Há basicamente dois tipos de erros
-- **Erro absoluto** em $v$ dado por
-```math
-E_A := \lvert u - v\rvert
-```
-- **Erro relativo** em $v$ dado por
-```math
-E_R := \frac{\lvert u - v\rvert}{\lvert u\rvert},
-```
-desde que $u\neq 0$. 
-* O erro relativo tem, em geral, mais significado, em especial, para erros de _representação de ponto flutuante_
-"""
-
-# ╔═╡ 0d2074aa-f918-4b6e-a7b3-1a2a84be333e
-u = [1, 1, -1.5, 100, 100]
-
-# ╔═╡ 5f980d0a-44e0-4d37-82a4-86c1e0a014e8
-v = [0.99, 1.01, -1.2, 99.99, 99]
-
-# ╔═╡ a37753de-9cb6-4d3d-84a7-087c8515886f
-md"""
----
-- Usando `DataFrames` para nossa tabela ficar bonitinha!
-"""
-
-# ╔═╡ 33db5da8-74a8-41df-ba17-8bc0251c824b
-DataFrame(:u=>u, :v=>v) #Erro absoluto e relativo
-
-# ╔═╡ 9fb230ba-dffc-4603-9c8d-00a1b1073d03
-md"""
-##### Aproximação de Stirling para fatorial de ${n}$
-A fórmula de Stirling diz que podemos utilizar 
-
-$$v = S_n = \sqrt{2\pi n} \left(\frac{n}{e}\right)^n$$
-
-para aproximar $v = n! = 1\cdot 2\cdot  \cdots \cdot n$, o fatoral de ${n}$ para ${n}$ grande.
-Usando a função `factorial(n)` de Julia vamos computar os erros relativos e absolutos de $S_n$, e apresentá-los em um `DataFrame`, para $1\leq n \leq 20$.
-"""
-
 # ╔═╡ Cell order:
-# ╟─a2d2d41c-d397-11eb-0f7d-ed2891f49049
+# ╠═a2d2d41c-d397-11eb-0f7d-ed2891f49049
+# ╟─09cb6899-07c6-436e-808f-b85f2c0f04de
 # ╟─4d8839cb-f01e-47b0-9671-3eb4f58e5acf
 # ╠═b25c1fd4-b6e2-4248-8bb9-41a1f06032dd
-# ╟─09cb6899-07c6-436e-808f-b85f2c0f04de
 # ╠═34ad60f7-8c7b-464c-8a67-1e9e6a562f84
 # ╠═602c26a1-0fda-482f-bcd1-adc2aa8b821c
 # ╠═529c5733-cdd0-4d04-8d15-d284d6cc436c
@@ -290,10 +233,6 @@ Usando a função `factorial(n)` de Julia vamos computar os erros relativos e ab
 # ╠═04cfba3e-91d5-48af-870b-6bfbb96b420e
 # ╠═f1d0e849-e5dd-439a-b4b5-a1f4b6228270
 # ╠═d0014623-085d-4162-b164-4ab360c84f18
-# ╠═ee4d5876-43ee-4f5a-951a-0ea3114ded2d
-# ╠═252c34fc-f179-45d9-8864-fc564ba013e5
-# ╠═aa5c435e-a069-452c-8a7f-d3ec19b844c6
-# ╠═fc7fc26e-15b9-47f2-861a-ac8793e0b1f0
 # ╟─80d793be-5d74-4254-8359-d5f029d203a4
 # ╠═7a9cbc0b-ca41-4850-ba76-550ac76bbefb
 # ╠═67674a24-ba42-4d0a-b6d1-30a9a07b94c7
@@ -302,9 +241,3 @@ Usando a função `factorial(n)` de Julia vamos computar os erros relativos e ab
 # ╠═d88a0f6f-d30c-4f2e-b1af-93d29ec60cc1
 # ╠═61fe0558-a535-400a-b44c-8f7455d529e0
 # ╠═edadaf37-303c-44b9-b73f-f972a9097198
-# ╟─206f3485-ba82-4b9e-948a-97e656f42b8a
-# ╠═0d2074aa-f918-4b6e-a7b3-1a2a84be333e
-# ╠═5f980d0a-44e0-4d37-82a4-86c1e0a014e8
-# ╟─a37753de-9cb6-4d3d-84a7-087c8515886f
-# ╠═33db5da8-74a8-41df-ba17-8bc0251c824b
-# ╟─9fb230ba-dffc-4603-9c8d-00a1b1073d03
