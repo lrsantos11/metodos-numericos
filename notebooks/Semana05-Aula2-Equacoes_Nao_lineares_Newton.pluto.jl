59c28d487157da64ad8fd5255721f95ddf204caa
Luiz-Rafael Santos <lrsantos11@gmail.com>
2021-07-14 18:01:06 -0300

    S05A02 First commit

---
+++ notebooks/Semana05-Aula2-Equacoes_Nao_lineares_Newton.pluto.jl
@@ -0,0 +1,22 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 597462d0-e4e6-11eb-1879-3522061d2d39
+md"""
+##### UFSC/Blumenau
+##### MAT1831 - Métodos Numéricos
+##### Prof. Luiz-Rafael Santos
+###### Semana 05 - Aula 02
+"""
+
+# ╔═╡ 387530b2-d156-488a-9f37-4a249da454c2
+md"""
+## Método de Newton-Raphson
+"""
+
+# ╔═╡ Cell order:
+# ╟─597462d0-e4e6-11eb-1879-3522061d2d39
+# ╠═387530b2-d156-488a-9f37-4a249da454c2
